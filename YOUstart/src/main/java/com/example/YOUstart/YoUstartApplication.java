6aaa3731fed8d86a8e5768f494e1a7c0582e03a8
testoccgg <testoccgg@gmail.com>
2023-05-03 06:47:27 +0300

    init greeting, home page

---
+++ YOUstart/src/main/java/com/example/YOUstart/YoUstartApplication.java
@@ -0,0 +1,12 @@
+package com.example.YOUstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoUstartApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(YoUstartApplication.class, args);
+	}
+
+}
---
+++ YOUstart/src/test/java/com/example/YOUstart/YoUstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YOUstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoUstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
