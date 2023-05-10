3bf06f0eac736b88070e4ef3725ef34c754f4bd4
testoccgg <testoccgg@gmail.com>
2023-05-10 19:03:02 +0300

    page user edit

--- YOUstart/src/main/java/com/example/YOUstart/controllers/UserController.java
+++ YOUstart/src/main/java/com/example/YOUstart/controllers/UserController.java
@@ -1,13 +1,20 @@
 package com.example.YOUstart.controllers;
 
+import com.example.YOUstart.mysql_struct.Role;
+import com.example.YOUstart.mysql_struct.User;
 import com.example.YOUstart.repos.UserRepo;
+import jakarta.persistence.ManyToOne;
 import jdk.jfr.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/users")
@@ -19,4 +26,29 @@ public class UserController {
         model.addAttribute("users",userRepo.findAll());
         return "userList";
     }
+    @GetMapping("/{user}")
+    public String editUser(@PathVariable User user, Model model){
+        model.addAttribute("user",user);
+        model.addAttribute("roles", Role.values());
+        return "editUser";
+    }
+
+    @PostMapping("/{user}")
+    public String editUserSave(@RequestParam String username,
+                               @RequestParam Map<String,String> form,
+                               @RequestParam("user_id") User user){
+        user.setUsername(username);
+
+        Set<String> roles= Arrays.stream(Role.values()).map(Role::name).collect(Collectors.toSet());
+
+        user.getRoles().clear();
+        form.keySet().forEach(x->{
+            if (roles.contains(x))user.getRoles().add(Role.valueOf(x));
+        });
+
+        userRepo.save(user);
+        return "redirect:/users";
+    }
+
+
 }
