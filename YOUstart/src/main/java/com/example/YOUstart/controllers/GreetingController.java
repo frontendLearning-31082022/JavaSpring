98bd809d422f7289abffc97dfa06a85af9a6a9a4
testoccgg <testoccgg@gmail.com>
2023-05-15 03:10:02 +0300

    completed: my messages, messages by user, edit message

--- YOUstart/src/main/java/com/example/YOUstart/controllers/GreetingController.java
+++ YOUstart/src/main/java/com/example/YOUstart/controllers/GreetingController.java
@@ -3,18 +3,21 @@ package com.example.YOUstart.controllers;
 import com.example.YOUstart.mysql_struct.Message;
 import com.example.YOUstart.mysql_struct.User;
 import com.example.YOUstart.repos.MessageRepo;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.HandlerMapping;
 
 import java.io.File;
 import java.io.IOException;
@@ -46,13 +49,12 @@ public class GreetingController {
         model.put("some","test");
         return "messages";}
 
-    @PostMapping("/messages")
+    @PostMapping(value = "/messages", params = "addSubmit")
     public String addMessages(@AuthenticationPrincipal User user,   @RequestParam("file")MultipartFile file,
                               @Valid Message message   ,
-                              BindingResult bindingResult, Model model) throws IOException {
+                              BindingResult bindingResult, Model model, HttpServletRequest request) throws IOException {
         message.setAuthor(user);
-
-        if(bindingResult.hasErrors()){
+          if(bindingResult.hasErrors()){
             Map<String, String> errorsMaps = ControllersUtils.getErrors(bindingResult);
             model.mergeAttributes(errorsMaps);
             model.addAttribute("message",message);
@@ -92,13 +94,50 @@ public class GreetingController {
 
 
     @GetMapping("/user-messages/messages/{userReq}")
-    public String userMessages(@AuthenticationPrincipal User currentUser,@PathVariable User userReq,Model model){
+    public String userMessages(@AuthenticationPrincipal User currentUser,
+                               @PathVariable User userReq,
+                               @RequestParam(required = false) Message message,
+                               Model model){
         Set<Message> messByUser=userReq.getMessages();
         model.addAttribute("messages",messByUser);
+        model.addAttribute("message",message);
 
         model.addAttribute("isCurrentUser",currentUser.equals(userReq));
 
         return "userMessages";
     }
+
+    @GetMapping(value = "/user-messages/messages/{userReq}/editMessage{message}")
+    public String editMessage(@AuthenticationPrincipal User currentUser,
+                               @PathVariable User userReq,
+                               @PathVariable Message message,
+                               Model model){
+        if (!currentUser.equals(message.getAuthor())){
+            model.addAttribute("errorMessage","Вы не являетесь владельцом сообщения");
+            return "error";
+        }
+
+
+        model.addAttribute("message",message);
+        return "editMessage";
+    }
+    @PostMapping(value = "/user-messages/messages/{userReq}/editMessage{message}")
+    public String editMessageSave(@AuthenticationPrincipal User currentUser,
+                              @PathVariable User userReq,
+                                  Message message,
+                              Model model){
+
+        if (!currentUser.equals(message.getAuthor())){
+            model.addAttribute("errorMessage","Вы не являетесь владельцом сообщения");
+            return "error";
+        }
+
+        messageRepo.save(message);
+        model.addAttribute("message",message);
+        model.addAttribute("result_saving","saved success");
+        return "editMessage";
+    }
+
+
 
 }
