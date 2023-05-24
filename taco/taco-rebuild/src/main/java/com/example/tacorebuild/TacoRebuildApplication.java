16690ae290b48845f802ffda3337c57ff99a0ec6
testoccgg <testoccgg@gmail.com>
2023-05-25 01:05:46 +0300

    init taco-rebuild

---
+++ taco/taco-rebuild/src/main/java/com/example/tacorebuild/TacoRebuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacorebuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoRebuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoRebuildApplication.class, args);
+	}
+
+}
---
+++ taco/taco-rebuild/src/test/java/com/example/tacorebuild/TacoRebuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacorebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoRebuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
