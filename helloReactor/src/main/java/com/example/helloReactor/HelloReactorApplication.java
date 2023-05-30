b94de37e4f10cdaaab54d35c1acfc399c57f2e47
testoccgg <testoccgg@gmail.com>
2023-05-30 16:47:28 +0300

    init

---
+++ helloReactor/src/main/java/com/example/helloReactor/HelloReactorApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloReactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloReactorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloReactorApplication.class, args);
+	}
+
+}
---
+++ helloReactor/src/test/java/com/example/helloReactor/HelloReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloReactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloReactorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
